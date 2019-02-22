3149ef85e8098a34f4e406de082d445e47f49541
chalizardmisadon <chalizardmisadon@yahoo.com>
2019-02-22 18:58:27 -0500

    update header and source file based on compile-safe code in /examples directory

--- MacRocketry_LED_Indicator.h
+++ MacRocketry_LED_Indicator.h
@@ -1,42 +1,28 @@
 #ifndef MacRocketry_LED_Indicator_h
 #define MacRocketry_LED_Indicator_h
 
-#define redPin 5
+#define redPin 9
 #define greenPin 6
-#define bluePin 3
+#define bluePin 5
 
-/*
-#define ERROR_ALL
-#define ERROR_BMP
-#define ERROR_
-*/
-
-
-
+#define errorLoopInterval 1000
 
 class MacRocketry_LED_Indicator {
   public:
-    MacRocketry_LED_Indicator(void);  //constructor
-    int StatusCheck(bool BMP_test, int fix, bool SD_check);
-    //bool CHECK_ALT(float BMP_alt, float GPS_alt); 
-  
+    MacRocketry_LED_Indicator(void); //constructor
+
+    bool Set_BMP_Status(bool stats); //check BMP error
+    bool Set_GPS_Status(int fix); //check GPS error
+    bool Set_SD_Status(bool stats); //check SD error
+
+    void displayLED(void);
+    
   private:
     bool BMP_Status;
     bool GPS_Status;
     bool SD_Status;
+    char error;
 
-    
-    enum loopError {
-      Error_BMP;
-      ERROR_GPS;
-      ERROR_SD;
-    };
-    loopError displayError;
-
-    int err;
-
-    bool CHECK_ALT(float BMP_alt, float GPS_alt);
-    void displayLED(int msg);
 };
 
 #endif
