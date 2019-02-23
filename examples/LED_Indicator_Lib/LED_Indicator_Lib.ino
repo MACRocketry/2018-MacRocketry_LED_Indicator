bc7d608ad7c7b80c52699321e4e3cbb9ecf1ec6d
chalizardmisadon <chalizardmisadon@yahoo.com>
2019-02-23 13:43:48 -0500

    change a bug where '0x' was used instead of '0b'

--- examples/LED_Indicator_Lib/LED_Indicator_Lib.ino
+++ examples/LED_Indicator_Lib/LED_Indicator_Lib.ino
@@ -42,70 +42,70 @@ MacRocketry_LED_Indicator::MacRocketry_LED_Indicator(){   //constructor
   GPS_Status = false;
   SD_Status = false;
 
-  error = 0x0; //reset error to all
+  error = 0b0; //0b means number is binary
   //bit set means working, bit clear means error
 
 }
 
 bool MacRocketry_LED_Indicator::Set_BMP_Status(bool stats){
   BMP_Status = stats;
-  if (BMP_Status) error |= 0x100; //set bit
-  else error &= 0x011; //clear bit
+  if (BMP_Status) error |= 0b100; //set bit
+  else error &= 0b011; //clear bit
   return BMP_Status;
 }
 
 bool MacRocketry_LED_Indicator::Set_GPS_Status(int fix){
   GPS_Status = 0 < fix;
-  if (GPS_Status) error |= 0x010; //set bit
-  else error &= 0x101; //clear bit
+  if (GPS_Status) error |= 0b010; //set bit
+  else error &= 0b101; //clear bit
   return GPS_Status;
 }
 
 bool MacRocketry_LED_Indicator::Set_SD_Status(bool stats){
   SD_Status = stats;
-  if (SD_Status) error |= 0x001; //set bit
-  else error &= 0x110; //clear bit
+  if (SD_Status) error |= 0b001; //set bit
+  else error &= 0b110; //clear bit
   return SD_Status;
 }
 
 
 void MacRocketry_LED_Indicator::displayLED(){
   switch (error){
-    case 0x000: //white --> BMP and GPS and SD errors
+    case 0b000: //white --> BMP and GPS and SD errors
       analogWrite(redPin, 255);
       analogWrite(bluePin, 255);
       analogWrite(greenPin, 255);
-    case 0x001: //red -> BMP and GPS errors
+    case 0b001: //red -> BMP and GPS errors
       analogWrite(redPin, 255);
       analogWrite(bluePin, 0);
       analogWrite(greenPin, 0);
       break;
-    case 0x010: //cyan -> BMP + SD error
+    case 0b010: //cyan -> BMP + SD error
       analogWrite(redPin, 0);
       analogWrite(bluePin, 255);
       analogWrite(greenPin, 255);
       break;
-    case 0x011: //purple -> BMP error
+    case 0b011: //purple -> BMP error
       analogWrite(redPin, 255);
       analogWrite(bluePin, 255);
       analogWrite(greenPin, 0);
       break;    
-    case 0x100: //cyan -> GPS + SD error
+    case 0b100: //cyan -> GPS + SD error
       analogWrite(redPin, 0);
       analogWrite(bluePin, 255);
       analogWrite(greenPin, 255);
       break;
-    case 0x101: //yellow -> GPS error
+    case 0b101: //yellow -> GPS error
       analogWrite(redPin, 255);
       analogWrite(bluePin, 0);
       analogWrite(greenPin, 255);
       break;
-    case 0x110: //blue -> SD error
+    case 0b110: //blue -> SD error
       analogWrite(redPin, 0);
       analogWrite(bluePin, 255);
       analogWrite(greenPin, 0);
       break;
-    case 0x111: //green -> no errors
+    case 0b111: //green -> no errors
       analogWrite(redPin, 0);
       analogWrite(bluePin, 0);
       analogWrite(greenPin, 255);
